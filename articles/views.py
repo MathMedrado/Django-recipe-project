f39697859077195be4e720d83396024f77413f11
Matheus Medrado <matheus-13-@hotmail.com>
2022-04-17 22:09:57 -0300

    Adicionando create form

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,6 @@
 from multiprocessing import context
+from turtle import title
+from venv import create
 from django.http import QueryDict
 from django.shortcuts import render
 from .models import Article
@@ -14,15 +16,25 @@ def article_search_view(request, *args, **kwargs):
         query = None
 
     article_obj = None
-
     if query is not None:
         article_obj = Article.objects.get(id=query)    
-        
     context ={
         'object' : article_obj
     }
     return render(request,"articles/search.html", context=context)
 
+def article_create_view(request, *args, **kwargs): 
+    context = {}
+    if request.method == 'POST':
+        form_data = request.POST
+        title = form_data.get('title')
+        content = form_data.get('content')
+        article_obj = Article.objects.create(title=title, content=content)
+        #ele cria esse objeto podemos armazena-lo em uma variavel e passar para a view
+        context['created'] = True
+        context['object'] = article_obj
+
+    return render(request, "articles/create.html", context=context)
 
 def article_detail_view(request, id=None, *args, **kwargs):
     article_obj = None
--- recipes/urls.py
+++ recipes/urls.py
@@ -17,12 +17,13 @@ import imp
 from django.contrib import admin
 from django.urls import path
 from .views import home_view
-from articles.views import article_detail_view, article_search_view
+from articles.views import article_detail_view, article_search_view, article_create_view
 #outra forma da invocação acima from article import views
 
 urlpatterns = [
     path('', home_view),
     path('articles/', article_search_view),
+    path('articles/create/', article_create_view),
     path('articles/<int:id>/', article_detail_view),
     #    path('article/<int:id>/', views.article_detail_view),
     path('admin/', admin.site.urls),
